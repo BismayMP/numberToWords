6781cda4b44a046a54ef6b216f1f28ad2589f4af
Bismay <bismaymp@gmail.com>
2021-07-09 01:31:37 -0400

    Rest endpoint

---
+++ api/src/routes/api.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import { validate, getWords, errorMessages } from '../controllers/api/index'
+
+const router = express.Router()
+
+/** Routes */
+// GET
+
+router.get('/:number', (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*')
+  if (validate(req?.params?.number)) {
+    const words = getWords(req.params.number)
+    res.status(200).json({ success: true, words })
+  } else {
+    res.status(400).json({ success: false, error: errorMessages.notValid })
+  }
+})
+
+export default router
